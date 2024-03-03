0103e2a7fa2874e3e8187b57a1f335dbba6eab4d
neritsuko <alina22ibraimova@gmail.com>
2024-03-03 18:08:08 +0500

    first commit

--- lab4/ViewController.swift
+++ lab4/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  lab4
-//
-//  Created by alina ibraimova on 03.03.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer: Timer?
+    var counter = 0
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Time: \(counter)"
     }
-
-
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        if timer == nil || !(timer?.isValid ?? false) {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func stopTimer(_ sender: UIButton) {
+        timer?.invalidate()
+    }
+    
+    @IBAction func resetTimer(_ sender: UIButton) {
+        counter = 0
+        label.text = "Time: \(counter)"
+        timer?.invalidate()
+    }
+    
+    @objc func updateTimer() {
+        counter += 1
+        label.text = "Time: \(counter)"
+    }
 }
-
